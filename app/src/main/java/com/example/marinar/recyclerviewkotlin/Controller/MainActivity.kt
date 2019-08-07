e102fa7df9d111b854aba3e7223955cb774b2063
Marina Romanova <mrnromanova@gmail.com>
2019-08-07 23:23:49 +0200

    created custom adapter for the list view

--- app/src/main/java/com/example/marinar/recyclerviewkotlin/Controller/MainActivity.kt
+++ app/src/main/java/com/example/marinar/recyclerviewkotlin/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.marinar.recyclerviewkotlin.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.marinar.recyclerviewkotlin.Adapters.CategoryAdapter
 import com.example.marinar.recyclerviewkotlin.Model.Category
 import com.example.marinar.recyclerviewkotlin.R
 import com.example.marinar.recyclerviewkotlin.Services.DataService
@@ -10,13 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }
